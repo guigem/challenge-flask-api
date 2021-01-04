b3578c365386d615c60495023a85c05eb58dc2ac
Guillaume <gemisguillaume@gmail.com>
2021-01-04 13:26:17 +0100

    Finishing routes

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 import random
 
@@ -7,12 +7,18 @@ app = Flask(__name__)
 
 @app.route("/status", methods=["GET"])
 def alive() -> str:
+    '''
+    This GET route checks the status of the API
 
+    Returns
+    -------
+    str
+        Return the string "alive".
+
+    '''
+    
     return "alive"
 
-
-# @app.route('/login', methods=["POST"])
-# def login():
 
 
 @app.route(
@@ -20,10 +26,51 @@ def alive() -> str:
     methods=["GET"],
 )
 def user(month: str, customer_visiting_website: int, seller_available: str) -> int:
+    '''
+    This GET route takes 3 parameters and returns a random number between 2000 and 5000.
+    
+    Parameters
+    ----------
+    month : str
+        Current month.
+    customer_visiting_website : int
+        Number of customers visiting the website for a given month.
+    seller_available : str
+        Name of the seller.
+
+    Returns
+    -------
+    int
+        Random number between 2000 and 5000.
+
+    '''
 
     random_num = random.randint(2000, 5000)
     random_num = str(random_num)
     return random_num
+
+
+@app.route("/login", methods=["POST"])
+def login() -> str:
+    
+    '''
+    This POST route makes sure that username and password are correct. 
+    The request is sent using postman.
+
+    Returns
+    -------
+    str
+        Returns a string with the correct information .
+
+    '''
+    
+    #Dictionnary with the json request for username and password
+    body = {
+            'user' : request.json["username"],
+            'password' : request.json["password"] }
+
+
+    return f"Login success for user {body['user']} with password of length: {len(body['password'])}!!"
 
 
 if __name__ == "__main__":
